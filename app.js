e9014d90939b6ddd964b01b443d2e8d73cfc23ec
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-09-27 08:21:37 -0500

    finished

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const searchBar = document.querySelector("input");
-const results = document.querySelector(".results");
+let search = document.querySelector("input");
+let recipes = document.querySelector(".container");
+
+search.addEventListener("keydown", function (e) {
+    let url = "http://recipepuppyproxy.herokuapp.com/api/?q=" + search.value;
+
+    if (event.keyCode === 13) {
+        axios.get(url).then(function (response) {
+            let data = response.data.results;
+            console.log(data);
+            data.forEach(function (data) {
+                let searchResults = ``;
+                searchResults = `
+            <div class="recipe" style="background-image: url(${data.thumbnail}), url(https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Dialog-error-round.svg/2000px-Dialog-error-round.svg.png)"><p class="title"><a href="${data.href}">${data.title}</a></p>
+            </div>`;
+
+                recipes.innerHTML += searchResults;
+            });
+        });
+    }
+});
 
